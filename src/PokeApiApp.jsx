ee79710ee0b09f85896bf871d8312a28c1e7d066
Erick Andrade <erick_andradhe@hotmail.com>
2023-04-24 11:54:48 -0600

    feat(pokemon): get information from pokeapi

--- src/PokeApiApp.jsx
+++ src/PokeApiApp.jsx
@@ -5,20 +5,58 @@ import { GeneratePokemon } from "./components/GeneratePokemon";
 
 export const PokeApiApp = () => {
 
-    const [pokemon, setPokemon] = useState([]);
+    // const [pokemon, setPokemon] = useState([]);
 
-    const onGetPokemon = ( newPokemon ) => {
+    // const onGetPokemon = ( newPokemon ) => {
         
-        setPokemon( newPokemon );
+    //     setPokemon( newPokemon );
+    // }
+
+
+    // Nombre
+    // Imagen
+    // Tipo
+    // peso
+    // altura
+
+    const [pokemon, setPokemon] = useState({})
+
+    const url = 'https://pokeapi.co/api/v2/pokemon/25';
+
+    async function getPokemon(){
+        const response = await fetch( url );
+        const data = await response.json();
+        console.log(data);
+
+        const test = {
+            'nombre': data.name,
+            'tipo': data.types[0].type.name,
+            'peso': data.weight,
+            'altura': data.height,
+            'hp': data.stats[0].base_stat,
+            'ataque': data.stats[1].base_stat,
+            'defensa': data.stats[2].base_stat,
+            'imagenPng': `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`,
+            'imagenGif': `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${data.id}.gif`,
+        };
+
+        console.log(test);
     }
+
+    getPokemon();
 
     return(
         <>
             <h1>Poke Api App</h1>
 
+
+
+
+
+
             {/* execute service and get information */}
 
-            <h1>{ pokemon }</h1>
+            {/* <h1>{ pokemon }</h1>
 
             <button
                 onClick={ onGenerate }
@@ -27,7 +65,7 @@ export const PokeApiApp = () => {
 
             <GeneratePokemon
                 onNewPokemon = { onGetPokemon }
-            />
+            /> */}
 
             {/* order information and show*/}
 
