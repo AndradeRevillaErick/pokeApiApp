09fb377988796a4a8255e77c1ed54ad76bdd5a96
Erick Andrade <erick_andradhe@hotmail.com>
2023-05-02 11:09:29 -0600

    feat(pokeapi): add file to get pokemon information from api

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -3,9 +3,22 @@
 export const Pokemon = ({ idPokemon }) => {
     return (
         <>
+        //recibe la informacion del pokemon no el id
             <h1>Pokemon: { idPokemon }</h1>
             //aqui se llama el servicio para obtener la info del pokemon
             //se muestra el pokemon
+            <h3>PokeInfo</h3>
+            <div>
+                <span>nombre: {}</span>
+                <span>tipo: {}</span>
+                <span>peso: {}</span>
+                <span>altura: {}</span>
+                <span>hp: {}</span>
+                <span>ataque: {}</span>
+                <span>defensa: {}</span>
+                <span>imagenPng: {}</span>
+                <span>imagenGif: {}</span>
+            </div>
         </>
     );
 }
