f04f5e858466cc7d9f6590c91cbc92f0cb3c2168
Erick Andrade <erick_andradhe@hotmail.com>
2023-05-11 13:18:47 -0600

    feat(hook): create usepokeapi to separate the logic

--- src/PokeApiApp.jsx
+++ src/PokeApiApp.jsx
@@ -1,63 +1,17 @@
-import { useEffect, useReducer, useState } from "react";
 import { Pokemon } from "./components/Pokemon";
-import { useFetchPokemon } from "./hooks/useFetchPokemon";
 import { PokemonList } from "./components/PokemonList";
-import { pokemonReducer } from "./helpers/pokemonReducer";
-
-const init = () => {
-  return JSON.parse(localStorage.getItem("pokemons") || []);
-};
-
-const initPoke = () => {
-  return JSON.parse(
-    localStorage.getItem("pokemon") || Math.floor(Math.random() * 1000)
-  );
-};
+import { usePokeapi } from "./hooks/usePokeapi";
 
 export const PokeApiApp = () => {
-  const [idPokemon, setIdPokemon] = useState(initPoke);
-  const { pokemon } = useFetchPokemon(idPokemon);
-  const [pokemons, dispatch] = useReducer(pokemonReducer, [], init);
-
-  useEffect(() => {
-    localStorage.setItem("pokemons", JSON.stringify(pokemons));
-  }, [pokemons]);
-
-  useEffect(() => {
-    localStorage.setItem("pokemon", idPokemon);
-  }, [idPokemon]);
-
-  function onGenerateRndId() {
-    const idPokemon = Math.floor(Math.random() * 1000);
-    setIdPokemon(idPokemon);
-  }
-
-  function handleAddPokemon(idPokemon) {
-    const action = {
-      type: "[POKEMON] Add pokemon",
-      payload: idPokemon,
-    };
-
-    dispatch(action);
-  }
-
-  function handleResetPokemon() {
-    const action = {
-      type: "[POKEMON] Reset pokemons",
-      payload: idPokemon,
-    };
-
-    dispatch(action);
-  }
-
-  function handleDeletePokemon(idPokemon) {
-    const action = {
-      type: "[POKEMON] Delete pokemon",
-      payload: idPokemon,
-    };
-
-    dispatch(action);
-  }
+  const {
+    pokemon,
+    pokemons,
+    idPokemon,
+    onGenerateRndId,
+    handleAddPokemon,
+    handleResetPokemon,
+    handleDeletePokemon,
+  } = usePokeapi();
 
   return (
     <>
